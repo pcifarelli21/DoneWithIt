593e5b0bc79099feb7370824f3028e438202e5cf
Peter <pcifarelli21@gmail.com>
2020-07-09 15:57:52 -0500

    create view image screen - refactor colors to seperate file

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
